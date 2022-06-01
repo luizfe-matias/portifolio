9b4b97012041fa36685d97f895cff906941ea830
Luiz Felipe <LuizFeMM@outlook.com>
2022-06-01 10:16:23 -0300

    Portifolio

--- index.php
+++ index.php
@@ -52,10 +52,10 @@ require_once('config.php');
             <ul>
                 <li><a href="#hero" class="nav-link scrollto active"><i class="bx bx-home"></i> <span>Home</span></a></li>
                 <li><a href="#about" class="nav-link scrollto"><i class="bx bx-user"></i> <span>Sobre</span></a></li>
-                 <li><a href="#facts" class="nav-link scrollto"><i class="bx bx-server"></i> <span>Fatos</span></a></li>
+                <li><a href="#facts" class="nav-link scrollto"><i class="bx bx-server"></i> <span>Fatos</span></a></li>
                 <li><a href="#resume" class="nav-link scrollto"><i class="bx bx-file-blank"></i> <span>Resumo</span></a></li>
                 <li><a href="#portfolio" class="nav-link scrollto"><i class="bx bx-book-content"></i> <span>Portfolio</span></a></li>
-               
+
                 <li><a href="#contact" class="nav-link scrollto"><i class="bx bx-envelope"></i> <span>Contato</span></a></li>
                 <!-- <li><a href="register.php" class="nav-link scrollto"><i class="bx bx-envelope"></i><span>Registrar Portifólio</span></a></li> -->
             </ul>
@@ -125,7 +125,7 @@ require_once('config.php');
                                             <li><i class="bi bi-chevron-right"></i> <strong>Cidade:</strong> <span><?php echo $row['cidade'] ?></span></li>
                                             <li><i class="bi bi-chevron-right"></i> <strong>Genero:</strong> <span><?php echo $row['genero'] ?></span></li>
 
-                                            <!-- <li><i for="nascimento" class="bi bi-chevron-right"></i> <strong>Nascimento:</strong><?php echo $row['nascimento'] ?><span></span></li> -->
+                                            q <li><i for="nascimento" class="bi bi-chevron-right"></i> <strong>Nascimento:</strong><?php echo $row['nascimento'] ?><span></span></li>
 
                                 </ul>
 
@@ -240,13 +240,13 @@ require_once('config.php');
                         </div>
 
                         <div class="row skills-content">
-                        <h1>oi</h1>
+
                             <div class="col-lg-6">
 
                                 <div class="progress">
                                     <span class="skill"><?php echo $row['nome_habilidades'] ?><i class="val"><?php echo $row['nivel']; ?>%</i></span>
                                     <div class="progress-bar-wrap">
-                                        <div class="progress-bar" role="progressbar" aria-valuenow="<?php echo $row['nivel']; ?>'" aria-valuemin="0" aria-valuemax="100"></div>
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
                                     </div>
                                 </div>
 
@@ -302,66 +302,74 @@ require_once('config.php');
         <!-- ======= Resume Section ======= -->
         <section id="resume" class="resume">
             <div class="container" data-aos="fade-up">
+                <?php
 
-                <div class="section-title">
-                    <h2>Resume</h2>
-                    <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
-                </div>
+                if (!empty($result)) {
+                    foreach ($result as $res2) {;
+                ?>
 
-                <div class="row">
-                    <div class="col-lg-6">
-                        <h3 class="resume-title">Sumary</h3>
-                        <div class="resume-item pb-0">
-                            <h4>Brandon Johnson</h4>
-                            <p><em>Innovative and deadline-driven Graphic Designer with 3+ years of experience designing and developing user-centered digital/print marketing material from initial concept to final, polished deliverable.</em></p>
-                            <ul>
-                                <li>Portland par 127,Orlando, FL</li>
-                                <li>(123) 456-7891</li>
-                                <li>alice.barkley@example.com</li>
-                            </ul>
+                        <div class="section-title">
+                            <h2>Resume</h2>
+                            <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
                         </div>
 
-                        <h3 class="resume-title">Education</h3>
-                        <div class="resume-item">
-                            <h4>Master of Fine Arts &amp; Graphic Design</h4>
-                            <h5>2015 - 2016</h5>
-                            <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
-                            <p>Qui deserunt veniam. Et sed aliquam labore tempore sed quisquam iusto autem sit. Ea vero voluptatum qui ut dignissimos deleniti nerada porti sand markend</p>
-                        </div>
-                        <div class="resume-item">
-                            <h4>Bachelor of Fine Arts &amp; Graphic Design</h4>
-                            <h5>2010 - 2014</h5>
-                            <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
-                            <p>Quia nobis sequi est occaecati aut. Repudiandae et iusto quae reiciendis et quis Eius vel ratione eius unde vitae rerum voluptates asperiores voluptatem Earum molestiae consequatur neque etlon sader mart dila</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <h3 class="resume-title">Professional Experience</h3>
-                        <div class="resume-item">
-                            <h4>Senior graphic design specialist</h4>
-                            <h5>2019 - Present</h5>
-                            <p><em>Experion, New York, NY </em></p>
-                            <ul>
-                                <li>Lead in the design, development, and implementation of the graphic, layout, and production communication materials</li>
-                                <li>Delegate tasks to the 7 members of the design team and provide counsel on all aspects of the project. </li>
-                                <li>Supervise the assessment of all graphic materials in order to ensure quality and accuracy of the design</li>
-                                <li>Oversee the efficient use of production project budgets ranging from $2,000 - $25,000</li>
-                            </ul>
-                        </div>
-                        <div class="resume-item">
-                            <h4>Graphic design specialist</h4>
-                            <h5>2017 - 2018</h5>
-                            <p><em>Stepping Stone Advertising, New York, NY</em></p>
-                            <ul>
-                                <li>Developed numerous marketing programs (logos, brochures,infographics, presentations, and advertisements).</li>
-                                <li>Managed up to 5 projects or tasks at a given time while under pressure</li>
-                                <li>Recommended and consulted with clients on the most appropriate graphic design</li>
-                                <li>Created 4+ design presentations and proposals a month for clients and account managers</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <h3 class="resume-title">Sumario</h3>
+                                <div class="resume-item pb-0">
+                                    <h4><?php echo $res2['nome']; ?></h4>
+                                    <p><em>Innovative and deadline-driven Graphic Designer with 3+ years of experience designing and developing user-centered digital/print marketing material from initial concept to final, polished deliverable.</em></p>
+                                    <ul>
+                                        <li>Portland par 127,Orlando, FL</li>
+                                        <li><?php echo $res2['telefone'] ;?></li>
+                                        <li><?php echo $res2['email'] ;?></li>
+                                    </ul>
+                                </div>
 
+                                <h3 class="resume-title">Education</h3>
+                                <div class="resume-item">
+                                    <h4>Master of Fine Arts &amp; Graphic Design</h4>
+                                    <h5>2015 - 2016</h5>
+                                    <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
+                                    <p>Qui deserunt veniam. Et sed aliquam labore tempore sed quisquam iusto autem sit. Ea vero voluptatum qui ut dignissimos deleniti nerada porti sand markend</p>
+                                </div>
+                                <div class="resume-item">
+                                    <h4>Bachelor of Fine Arts &amp; Graphic Design</h4>
+                                    <h5>2010 - 2014</h5>
+                                    <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
+                                    <p>Quia nobis sequi est occaecati aut. Repudiandae et iusto quae reiciendis et quis Eius vel ratione eius unde vitae rerum voluptates asperiores voluptatem Earum molestiae consequatur neque etlon sader mart dila</p>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <h3 class="resume-title">Professional Experience</h3>
+                                <div class="resume-item">
+                                    <h4>Senior graphic design specialist</h4>
+                                    <h5>2019 - Present</h5>
+                                    <p><em>Experion, New York, NY </em></p>
+                                    <ul>
+                                        <li>Lead in the design, development, and implementation of the graphic, layout, and production communication materials</li>
+                                        <li>Delegate tasks to the 7 members of the design team and provide counsel on all aspects of the project. </li>
+                                        <li>Supervise the assessment of all graphic materials in order to ensure quality and accuracy of the design</li>
+                                        <li>Oversee the efficient use of production project budgets ranging from $2,000 - $25,000</li>
+                                    </ul>
+                                </div>
+                                <div class="resume-item">
+                                    <h4>Graphic design specialist</h4>
+                                    <h5>2017 - 2018</h5>
+                                    <p><em>Stepping Stone Advertising, New York, NY</em></p>
+                                    <ul>
+                                        <li>Developed numerous marketing programs (logos, brochures,infographics, presentations, and advertisements).</li>
+                                        <li>Managed up to 5 projects or tasks at a given time while under pressure</li>
+                                        <li>Recommended and consulted with clients on the most appropriate graphic design</li>
+                                        <li>Created 4+ design presentations and proposals a month for clients and account managers</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                <?php
+                    }
+                }
+                ?>
             </div>
         </section><!-- End Resume Section -->
 
@@ -380,7 +388,7 @@ require_once('config.php');
                             <li data-filter="*" class="filter-active">All</li>
                             <li data-filter=".filter-app">People</li>
                             <li data-filter=".filter-card">Camera</li>
-                            <li data-filter=".filter-web">Celular</li>
+                            <li data-filter=".filter-web">Landscape</li>
                         </ul>
                     </div>
                 </div>
@@ -400,7 +408,33 @@ require_once('config.php');
                             </div>
                         </div>
                     </div>
-
+                    
+                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+                        <div class="portfolio-wrap">
+                            <img src="assets/img/portfolio/motor.jpeg" class="img-fluid" alt="">
+                            <div class="portfolio-info">
+                                <h4>Pessoal</h4>
+                                <p>Iphone</p>
+                                <div class="portfolio-links">
+                                    <a href="assets/img/portfolio/motor.jpeg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Sunset"><i class="bx bx-plus"></i></a>
+                                    <!-- <a href="portfolio-details.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+                        <div class="portfolio-wrap">
+                            <img src="assets/img/portfolio/Purple-sky.JPG" class="img-fluid" alt="">
+                            <div class="portfolio-info">
+                                <h4>Camera</h4>
+                                <p>Canon EOS T7</p>
+                                <div class="portfolio-links">
+                                    <a href="assets/img/portfolio/Purple-sky.JPG" data-gallery="portfolioGallery" class="portfolio-lightbox" title="A beautiful sunset"><i class="bx bx-plus"></i></a>
+                                    <!-- <a href="portfolio-details.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <div class="col-lg-4 col-md-6 portfolio-item filter-web">
                         <div class="portfolio-wrap">
                             <img src="assets/img/portfolio/Field.jpeg" class="img-fluid" alt="">
@@ -429,19 +463,7 @@ require_once('config.php');
                         </div>
                     </div>
 
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-                        <div class="portfolio-wrap">
-                            <img src="assets/img/portfolio/Purple-sky.JPG" class="img-fluid" alt="">
-                            <div class="portfolio-info">
-                                <h4>Camera</h4>
-                                <p>Canon EOS T7</p>
-                                <div class="portfolio-links">
-                                    <a href="assets/img/portfolio/Purple-sky.JPG" data-gallery="portfolioGallery" class="portfolio-lightbox" title="A beautiful sunset"><i class="bx bx-plus"></i></a>
-                                    <!-- <a href="portfolio-details.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a> -->
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    
 
                     <div class="col-lg-4 col-md-6 portfolio-item filter-web">
                         <div class="portfolio-wrap">
